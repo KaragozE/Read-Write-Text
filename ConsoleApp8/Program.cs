82c862a8f9bc1a6f47dafa1751557c727a18d8f3
KaragozE <karagoz.eray@gmail.com>
2018-11-27 11:30:00 +0300

    first commit

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string read1 = System.IO.File.ReadAllText(@"C:\Users\ekaragoz\Desktop\read1.txt");
+            string read2 = System.IO.File.ReadAllText(@"C:\Users\ekaragoz\Desktop\read2.txt");
+
+            string write = read1 +"\t and \t"+ read2;
+
+            System.IO.File.WriteAllText(@"C:\Users\ekaragoz\Desktop\write.txt",write);
+            
+            Console.ReadKey();
+        }
+    }
+}
